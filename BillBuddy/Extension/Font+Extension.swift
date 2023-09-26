425553eb5ed4ee349f122c6d666acb9f1c5d9b52
yoonjiho37 <yoonjiho37@github.com>
2023-09-26 10:31:41 +0900

    add: 폰트, date, 컬러, 파일위치

---
+++ BillBuddy/Extension/Font+Extension.swift
@@ -0,0 +1,59 @@
+//
+//  Font+Extension.swift
+//  BillBuddy
+//
+//  Created by 윤지호 on 2023/09/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static var headline1: Font {
+        return Font.custom("Pretendard-Bold", size: 40)
+    }
+    
+    static var title01: Font {
+        return Font.custom("Pretendard-Bold", size: 38)
+    }
+    
+    static var title02: Font {
+        return Font.custom("Pretendard-Bold", size: 36)
+    }
+    
+    static var title03: Font {
+        return Font.custom("Pretendard-Bold", size: 32)
+    }
+    
+    static var title04: Font {
+        return Font.custom("Pretendard-Bold", size: 26)
+    }
+    
+    static var title05: Font {
+        return Font.custom("Pretendard-Bold", size: 18)
+    }
+    
+    static var body01: Font {
+        return Font.custom("Pretendard-SemiBold", size: 16)
+    }
+    
+    static var body02: Font {
+        return Font.custom("Pretendard-Bold", size: 14)
+    }
+    
+    static var body03: Font {
+        return Font.custom("Pretendard-SemiBold", size: 14)
+    }
+    
+    static var body04: Font {
+        return Font.custom("Pretendard-Medium", size: 14)
+    }
+    
+    static var caption01: Font {
+        return Font.custom("Pretendard-Bold", size: 12)
+    }
+    
+    static var caption02: Font {
+        return Font.custom("Pretendard-Bold", size: 10)
+    }
+}
