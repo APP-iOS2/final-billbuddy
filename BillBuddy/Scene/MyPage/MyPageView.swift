9eea2e85b38c5f378622f6da7b91a7555c25f4c3
wowhyunnie <wowhyunnie@gmail.com>
2023-09-27 12:07:06 +0900

    feat: 로그인, 회원가입, 마이페이지 뷰
    로그인, 회원가입 스토어 추가

--- BillBuddy/Scene/MyPage/MyPageView.swift
+++ BillBuddy/Scene/MyPage/MyPageView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct MyPageView: View {
+    
+    var user : User
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section("내정보") {
+                HStack {
+                    Image(systemName: "person.crop.circle")
+                    Text(user.name)
+                }
+                .font(.largeTitle)
+                Text(user.bankName)
+                Text(user.bankAccountNum)
+            }
+            
+            Section("설정") {
+                Text("알림 설정")
+                Text("위치 설정")
+            }
+            Section("정보") {
+                Text("개인정보 이용 동의")
+                Text("문의하기")
+                Text("오픈소스 라이센스")
+            }
+            Section {
+                Text("로그아웃")
+                Text("계정탈퇴")
+                    .foregroundStyle(.red)
+            }
+        }
     }
 }
 
 #Preview {
-    MyPageView()
+    
+    MyPageView(user: User(email: "이메일", name: "이름", phoneNum: "휴대폰 번호", bankName: "은행", bankAccountNum: "계좌 번호", isPremium: false, premiumDueDate: 0))
+    
 }
