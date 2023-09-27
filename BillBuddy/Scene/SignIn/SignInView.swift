99809ab2254eac878aa149d70fe05b4cfa358dde
yoonjiho37 <109410688+yoonjiho37@users.noreply.github.com>
2023-09-27 13:14:08 +0900

    Revert "feat: 로그인, 회원가입, 마이페이지 뷰"

--- BillBuddy/Scene/SignIn/SignInView.swift
+++ BillBuddy/Scene/SignIn/SignInView.swift
@@ -8,33 +8,11 @@
 import SwiftUI
 
 struct SignInView: View {
-    
-    @ObservedObject var signInStore: SignInStore
-    
     var body: some View {
-        Form {
-            Section("로그인") {
-                TextField("이메일", text: $signInStore.emailText)
-                SecureField("비밀번호", text: $signInStore.passwordText)
-            }
-            
-            Button {
-                //
-            } label: {
-                Text("로그인")
-            }
-            
-            NavigationLink {
-                SignUpView(signUpstore: SignUpStore())
-            } label: {
-                Text("회원가입")
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NavigationStack {
-        SignInView(signInStore: SignInStore())
-    }
+    SignInView()
 }
