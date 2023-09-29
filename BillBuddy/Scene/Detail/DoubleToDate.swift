a804dabd6c627adab73a64e3381be9dc51779f63
usingkim <kimyujin173@gmail.com>
2023-09-29 15:42:34 +0900

    add: extension Double File

--- BillBuddy/Scene/Detail/DoubleToDate.swift
+++ BillBuddy/Scene/Detail/DoubleToDate.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+extension Double {
+    func toDate() -> Date {
+        return Date(timeIntervalSince1970: self)
+    }
+    
+    func toFormattedDate() -> String {
+        let dateCreatedAt: Date = Date(timeIntervalSince1970: self)
+        
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_kr")
+        dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+        dateFormatter.dateFormat = "YY년 MM월 dd일 HH시 mm분"
+        
+        return dateFormatter.string(from: dateCreatedAt)
+
+    }
+}
