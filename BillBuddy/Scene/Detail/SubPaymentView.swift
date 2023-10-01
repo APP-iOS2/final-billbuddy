053c74bd13bde6beb50551903a37a3d107462d64
usingkim <kimyujin173@gmail.com>
2023-10-01 18:42:32 +0900

    feat: 지출 항목 추가/수정 뷰 인원 추가/삭제

--- BillBuddy/Scene/Detail/SubPaymentView.swift
+++ BillBuddy/Scene/Detail/SubPaymentView.swift
@@ -29,10 +29,28 @@ struct AddPaymentMemberView: View {
             .padding()
             .sheet(isPresented: $isShowingAddSheet, content: {
                 List(memberStore.members) { member in
-                    Text(member.name)
-                        .onTapGesture {
-                            newMembers.append(member)
+                    HStack {
+                        Text(member.name)
+                            .onTapGesture {
+                                // TODO: firstIndex로 두번이나 찾으면 메모리 너무 많이 먹는거 아닌가
+                                // 각각에 대해서 배열로 만들수도 없고 이걸 어뚜케 해야하지? 나중에 Refactoring 고민해보기!
+                                if let existMember = newMembers.firstIndex(where: { m in
+                                    m.name == member.name
+                                }) {
+                                    newMembers.remove(at: existMember)
+                                }
+                                else {
+                                    newMembers.append(member)
+                                }
+                            }
+                        Spacer()
+                        if let existMember = newMembers.firstIndex(where: { m in
+                            m.name == member.name
+                        }) {
+                            Image(systemName: "checkmark.seal.fill")
                         }
+                        
+                    }
                 }
                 .onAppear {
                     memberStore.fetchAll()
