b171a989bd34a923cc5179d3baee80de0d077940
seungzunlee <dltmdwns312@gmail.com>
2023-10-04 17:40:21 +0900

    feat: MapView

--- BillBuddy/Scene/Map/MapView.swift
+++ BillBuddy/Scene/Map/MapView.swift
@@ -2,7 +2,6 @@
 //  MapView.swift
 //  BillBuddy
 //
-//  Created by 윤지호 on 2023/09/22.
 //
 
 import SwiftUI
@@ -13,20 +12,36 @@ struct MapView: View {
     @State private var searchText: String = ""
     
     var body: some View {
-        VStack {
-            HStack {
-                TextField("주소를 입력하세요", text: $searchText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                Button(action: {
-                    locationManager.searchAddress(searchText: searchText)
-                }, label: {
-                    Text("추가")
-                })
-                .padding()
-            }
+        GeometryReader { geometry in
             MapViewCoordinater(locationManager: locationManager)
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.white)
+                        .frame(width: geometry.size.width, height: 40)
+                        .shadow(radius: 2, y: 1)
+                        .overlay(alignment: .leading) {
+                            HStack {
+                                TextField("주소를 입력하세요", text: $searchText)
+                                    .padding()
+                                Button(action: {
+                                    locationManager.searchAddress(searchText: searchText)
+                                }, label: {
+                                    Text("추가")
+                                })
+                                .padding()
+                            }
+                        }
+                }
+            }
+            Image("DBPin")
+                .resizable()
+                .position(CGPoint(x: geometry.size.width / 2 + 7, y: locationManager.isChaging ? (geometry.size.height / 2 - 25) : (geometry.size.height / 2 - 20)))
+                .frame(width: 50, height: 50, alignment: .center)
         }
+//        .onAppear {
+//            locationManager.fetchAnotations()
+//        }
     }
 }
 
