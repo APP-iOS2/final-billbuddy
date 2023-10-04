e684702f0537077a0698793c105be7ddfeb47db4
yoonjiho37 <yoonjiho37@github.com>
2023-10-04 20:36:17 +0900

    chore: package setting

--- BillBuddy/Scene/TravelList/Views/AddTravel/AddTravelView.swift
+++ BillBuddy/Scene/TravelList/Views/AddTravel/AddTravelView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct AddTravelView: View {
     @Binding var travelData: TravelCalculation
     @EnvironmentObject var userTravelStore: UserTravelStore
-    @State var newTravel = TravelCalculation(hostId: "", travelTitle: "", managerId: "", startDate: Date(), endDate: Date(), updateContentDate: Date(), members: [])
+    @State private var newTravel = TravelCalculation(hostId: "", travelTitle: "", managerId: "", startDate: Date().timeIntervalSince1970, endDate: Date().timeIntervalSince1970, updateContentDate: Date(), members: [])
     @State private var selectedMember = 0
+    @State private var startDate: Date = Date()
+    @State private var endDate: Date = Date()
+    
     
     var body: some View {
         VStack {
@@ -20,11 +23,11 @@ struct AddTravelView: View {
                 Text("여행 제목")
                 TextField("여행 제목을 입력해주세요.", text: $travelData.travelTitle)
                 
-                DatePicker("시작 일", selection: $travelData.startDate, displayedComponents: [.date])
+                DatePicker("시작 일", selection: $startDate, displayedComponents: [.date])
                     .datePickerStyle(.automatic)
                     .padding(.bottom, 10)
                 
-                DatePicker("종료 일", selection: $travelData.endDate, displayedComponents: [.date])
+                DatePicker("종료 일", selection: $endDate, displayedComponents: [.date])
                     .datePickerStyle(.automatic)
             }
             .padding([.leading, .trailing], 12)
@@ -48,6 +51,7 @@ struct AddTravelView: View {
         Spacer()
         
         Button {
+            
             userTravelStore.addTravel(newTravel)
             
         } label: {
@@ -57,6 +61,6 @@ struct AddTravelView: View {
 }
 
 #Preview {
-    AddTravelView(travelData: .constant(TravelCalculation(hostId: "", travelTitle: "", managerId: "", startDate: Date(), endDate: Date(), updateContentDate: Date(), members: [])))
+    AddTravelView(travelData: .constant(TravelCalculation(hostId: "", travelTitle: "", managerId: "", startDate: Date().timeIntervalSince1970, endDate: Date().timeIntervalSince1970, updateContentDate: Date(), members: [])))
            .environmentObject(UserTravelStore())
 }
