bc3ce8f35e6cbeffe4325bdf1da5d8326409d4dd
usingkim <kimyujin173@gmail.com>
2023-10-04 14:45:29 +0900

    add: MoreView / NotificationListView

---
+++ BillBuddy/Scene/Detail/NotificationListView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationListView.swift
+//  BillBuddy
+//
+//  Created by 김유진 on 10/4/23.
+//
+
+import SwiftUI
+
+struct NotificationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationListView()
+}
