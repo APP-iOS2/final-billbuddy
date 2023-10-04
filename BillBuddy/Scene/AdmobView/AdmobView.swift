429288e78d7d915966cb0359498dbb8617c314de
SangIn Kim <sikim4991@gmail.com>
2023-10-04 17:48:56 +0900

    배너 광고 구글 코드로 대체 및 네이티브 광고 테스트 중

--- BillBuddy/Scene/AdmobView/AdmobView.swift
+++ BillBuddy/Scene/AdmobView/AdmobView.swift
@@ -9,35 +9,38 @@ import SwiftUI
 import GoogleMobileAds
 
 
-struct GADBanner: UIViewControllerRepresentable {
-    
-    func makeUIViewController(context: Context) -> some UIViewController {
-        let view = GADBannerView(adSize: GADAdSizeBanner)
-        let viewController = UIViewController()
-        view.adUnitID = "ca-app-pub-3940256099942544/2934735716" // test Key
-        view.rootViewController = viewController
-        viewController.view.addSubview(view)
-        viewController.view.frame = CGRect(origin: .zero, size: GADAdSizeBanner.size)
-        view.load(GADRequest())
-        return viewController
-    }
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-    
-    }
-}
+//struct GADBanner: UIViewControllerRepresentable {
+//
+//    func makeUIViewController(context: Context) -> some UIViewController {
+//        let view = GADBannerView(adSize: GADAdSizeBanner)
+//        let viewController = UIViewController()
+//        view.adUnitID = "ca-app-pub-3940256099942544/2934735716" // test Key
+//        view.rootViewController = viewController
+//        viewController.view.addSubview(view)
+//        viewController.view.frame = CGRect(origin: .zero, size: GADAdSizeBanner.size)
+//        view.load(GADRequest())
+//        return viewController
+//    }
+//    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+//
+//    }
+//}
+
+
+// Delegate methods for receiving width update messages.
+
+
 
 struct AdmobView: View {
-//    @StateObject private var viewModel = ViewModel()
+    //    @StateObject private var viewModel = ViewModel()
     
     var body: some View {
         VStack {
-            Spacer()
-            Text("남의 코드 가져오는 게 체고야!")
-            Spacer()
+            BannerView()
             ///배너광고 뷰
-            GADBanner().frame(width: GADAdSizeBanner.size.width, height: GADAdSizeBanner.size.height)
-            ///Native 광고 테스트 중
-//            NativeAdView(nativeAdViewModel: viewModel)
+            //                        GADBanner().frame(width: GADAdSizeBanner.size.width, height: GADAdSizeBanner.size.height)
+            //            ///Native 광고 테스트 중
+            //                        NativeAdView(nativeAdViewModel: viewModel)
         }
     }
 }
