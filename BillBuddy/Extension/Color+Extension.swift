cce00b31cc5d4b97ec78bbd736a3444972aca204
yoonjiho37 <yoonjiho37@github.com>
2023-10-09 06:09:11 +0900

    refactor: Color Extension Code 고침

--- BillBuddy/Extension/Color+Extension.swift
+++ BillBuddy/Extension/Color+Extension.swift
@@ -17,7 +17,7 @@ extension Color {
         
         Scanner(string: hexSanitized).scanHexInt64(&rgb)
         
-        let red = Double((rgb & 0xFF000) >> 16) / 255.0
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
         let green = Double((rgb & 0x00FF00) >> 8) / 255.0
         let blue = Double(rgb & 0x0000FF) / 255.0
         
