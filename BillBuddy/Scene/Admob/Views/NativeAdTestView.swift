fe56e85b314de34a2dd55586829b7ef1a1a80198
SangIn Kim <sikim4991@gmail.com>
2023-10-10 01:51:14 +0900

    NativeAd UIKit 구현 완료 (Ad 속성 이슈있음), 코드 분할 및 주석 등 정리필요

---
+++ BillBuddy/Scene/Admob/Views/NativeAdTestView.swift
@@ -0,0 +1,20 @@
+//
+//  NativeAdTestView.swift
+//  BillBuddy
+//
+//  Created by SIKim on 10/9/23.
+//
+
+import UIKit
+
+class NativeAdTestView: GADNativeAdView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
