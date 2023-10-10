53458e4416f3f52137ecac18efa0b01e541a8b47
Jiyeon Hwang <46298003+growlamb@users.noreply.github.com>
2023-10-10 11:45:05 +0900

    Update ChatNotifincationCell.swift

--- BillBuddy/Scene/Membership/Notification/Views/ChatNotifincationCell.swift
+++ BillBuddy/Scene/Membership/Notification/Views/ChatNotifincationCell.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ChatNotifincationCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 25) {
+            NavigationLink {
+                
+            } label: {
+                knockNotificationLabel
+            }
+        }
+        .padding(.top, 10)
+        .padding(.horizontal, 20)
+    }
+    
+    private var knockNotificationLabel: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("\("UserName")")
+                Text("Chatting Content")
+            }
+            
+            Spacer()
+            
+            VStack {
+                Spacer()
+                Text("\("1") 시간 전")
+            }
+        }
     }
 }
 
