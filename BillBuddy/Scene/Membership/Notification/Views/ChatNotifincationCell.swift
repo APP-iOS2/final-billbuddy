350bf48b11a90e6d4afb82dc8b20776d5fa0edda
Jiyeon Hwang <46298003+growlamb@users.noreply.github.com>
2023-10-10 11:18:17 +0900

    add: ChatNotifincationCell 추가

---
+++ BillBuddy/Scene/Membership/Notification/Views/ChatNotifincationCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatNotifincationCell.swift
+//  BillBuddy
+//
+//  Created by hj on 2023/10/10.
+//
+
+import SwiftUI
+
+struct ChatNotifincationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatNotifincationCell()
+}
