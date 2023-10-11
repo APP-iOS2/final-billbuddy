52c87d4cbfa9da384d62d994a3ab3dab54710152
yuri rho <yforyurirho@gmail.com>
2023-10-11 15:50:17 +0900

    feat: 채팅방/메세지 모델 작성

---
+++ BillBuddy/Scene/Chatting/Model/Chat.swift
@@ -0,0 +1,31 @@
+//
+//  Chat.swift
+//  BillBuddy
+//
+//  Created by yuri rho on 2023/10/11.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+// MARK: 채팅방 모델
+struct Chat: Identifiable, Codable {
+    
+    // 채팅방 ID
+    var id: String = UUID().uuidString
+    // 채팅방 타이틀
+    let travelTitle: String
+    // 여행 생성한 멤버 ID
+    let hostId: String
+    // 채팅방 생성 날짜
+    let createdDate: Double
+    // 채팅에 참여한 유저 ID
+    let memberIDs: [Member]
+    // 마지막 메세지 내용 - 미리보기
+    var lastMessage: String
+    // 마지막 메세지 날짜 -- 미리보기, 리스트 정렬 순서
+    var lastMessageDate: Date
+    // 읽지 않은 메세지 수 [유저아이디 : 갯수]
+    var unreadMessageCount: [String : Int]
+        
+}
