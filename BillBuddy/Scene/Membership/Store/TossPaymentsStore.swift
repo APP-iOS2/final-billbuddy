d9d5edd23f90ebccd4ab23577bc64e384bc982ee
SangIn Kim <sikim4991@gmail.com>
2023-10-15 03:15:39 +0900

    토스 페이먼츠 구현 완료 ( 주석 필요 )

---
+++ BillBuddy/Scene/Membership/Store/TossPaymentsStore.swift
@@ -0,0 +1,8 @@
+//
+//  TossPaymentsStore.swift
+//  BillBuddy
+//
+//  Created by SIKim on 10/12/23.
+//
+
+import Foundation
--- BillBuddy/Scene/Membership/Views/TossPaymentsView.swift
+++ BillBuddy/Scene/Membership/Views/TossPaymentsView.swift
@@ -1,5 +1,5 @@
 //
-//  MembershipView.swift
+//  TossPaymentsView.swift
 //  BillBuddy
 //
 //  Created by SIKim on 10/12/23.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MembershipView: View {
+struct TossPaymentsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MembershipView()
+    TossPaymentsView()
 }
