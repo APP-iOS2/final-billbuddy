eeb31def51dda22ef68abbcf451ce57556db723f
Jiyeon Hwang <46298003+growlamb@users.noreply.github.com>
2023-10-17 01:13:41 +0900

    chore: FirebaseMessaging 패키지 추가, 빌드 Firebase 토큰 생성, 프로젝트 Push Notifications추가

--- BillBuddy/Scene/App/BillBuddyApp.swift
+++ BillBuddy/Scene/App/BillBuddyApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 import GoogleMobileAds
+import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
@@ -15,7 +16,30 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         FirebaseApp.configure()
         ///모바일 광고 SDK 초기화
         GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
+        /// 원격 알림 등록
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        
+        ///메시징 덜리겟
+        Messaging.messaging().delegate = self
+        
+        /// 푸시 포그라운드 설정
+        UNUserNotificationCenter.current().delegate = self
+        
         return true
+    }
+    
+    /// FCM 토근이 등록 되었을대
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
     }
 }
 
@@ -30,5 +54,40 @@ struct BillBuddyApp: App {
                     SchemeService.shared.getUrl(url: url)
                 })
         }
+    }
+}
+
+extension AppDelegate : MessagingDelegate {
+    
+    /// FCM 등록 토큰을 받았을 때
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("AppDelegate - 파베 토큰을 받았습니다.")
+        print("AppDelegate - Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
+
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    /// 푸시메시지가 앱이 커져 있을때 나올때
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        let userInfo = notification.request.content.userInfo
+        
+        print("willPresent: userInfo: ", userInfo)
+        
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    /// 푸시메시지를 받았을 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        print("didReceivet: userInfo: ", userInfo)
+        completionHandler()
+        
     }
 }
