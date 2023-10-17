1bd5c7a82f6dbed3c13e91a119c4dc7c3bf9aa5f
yoonjiho37 <yoonjiho37@github.com>
2023-10-18 07:09:01 +0900

    feat: TabView, DeepLinkView

---
+++ BillBuddy/Scene/DeepLink/DeepLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  DeepLinkView.swift
+//  BillBuddy
+//
+//  Created by 윤지호 on 10/18/23.
+//
+
+import SwiftUI
+
+struct DeepLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeepLinkView()
+}
