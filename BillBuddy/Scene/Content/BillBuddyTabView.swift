958394b77505c87899c9ad1acef8b396ab132d3b
SangIn Kim <sikim4991@gmail.com>
2023-10-19 13:48:02 +0900

    (feat: 프리미엄 혜택 적용 완료, 배너광고 및 전체화면 광고 추가)

--- BillBuddy/Scene/Content/BillBuddyTabView.swift
+++ BillBuddy/Scene/Content/BillBuddyTabView.swift
@@ -10,9 +10,11 @@ import UIKit
 
 struct BillBuddyTabView: View {
     @State private var selectedTab = 0
+    @State private var isShowingAdScreen: Bool = false
     @StateObject private var floatingButtonMenuStore = FloatingButtonMenuStore()
 //    @State var isDimmedBackground = false
-
+    @EnvironmentObject private var userService: UserService
+    
     init() {
         UITabBar.appearance().unselectedItemTintColor = UIColor(Color.gray500)
         UITabBarItem.appearance().setTitleTextAttributes([.font:UIFont(name: "Pretendard-Bold", size: 10)!], for: .normal)
@@ -22,7 +24,16 @@ struct BillBuddyTabView: View {
         TabView(selection: $selectedTab) {
             NavigationStack {
                 TravelListView(floatingButtonMenuStore: floatingButtonMenuStore)
-                    
+                if let isPremium = userService.currentUser?.isPremium {
+                    if isPremium == false {
+                        BannerView().frame(height: 65)
+                            .overlay(
+                                Rectangle()
+                                    .fill(Color.systemBlack.opacity(floatingButtonMenuStore.isDimmedBackground ? 0.5 : 0)).edgesIgnoringSafeArea(.all)
+                            )
+                            .padding(.top, -8)
+                    }
+                }
             }
             .tabItem {
                 Image(.hometap)
@@ -31,27 +42,62 @@ struct BillBuddyTabView: View {
             }
 //            .brightness(isDimmedBackground ? -0.5 : 0)
             .toolbarBackground(Color.systemBlack.opacity(floatingButtonMenuStore.isDimmedBackground ? 0.5 : 0), for: .tabBar)
+            .fullScreenCover(isPresented: $isShowingAdScreen, content: {
+                NativeContentView(isShowingAdScreen: $isShowingAdScreen)
+            })
+            .onAppear {
+                if let isPremium = userService.currentUser?.isPremium {
+                    isShowingAdScreen = Bool.random()
+                }
+            }
             .tag(0)
             
             NavigationStack {
                 ChattingView()
+                if let isPremium = userService.currentUser?.isPremium {
+                    if isPremium == false {
+                        BannerView().frame(height: 60)
+                            .padding(.top, -8)
+                    }
+                }
             }
             .tabItem {
                 Image(.chattap)
                     .renderingMode(.template)
                 Text("채팅")
             }
+            .fullScreenCover(isPresented: $isShowingAdScreen, content: {
+                NativeContentView(isShowingAdScreen: $isShowingAdScreen)
+            })
+            .onAppear {
+                if let isPremium = userService.currentUser?.isPremium {
+                    isShowingAdScreen = Bool.random()
+                }
+            }
             .tag(1)
             
             NavigationStack {
                 MyPageView()
+                if let isPremium = userService.currentUser?.isPremium {
+                    if isPremium == false {
+                        BannerView().frame(height: 60)
+                            .padding(.top, -8)
+                    }
+                }
             }
             .tabItem {
                 Image(.mypagetap)
                     .renderingMode(.template)
                 Text("마이페이지")
             }
-
+            .fullScreenCover(isPresented: $isShowingAdScreen, content: {
+                NativeContentView(isShowingAdScreen: $isShowingAdScreen)
+            })
+            .onAppear {
+                if let isPremium = userService.currentUser?.isPremium {
+                    isShowingAdScreen = Bool.random()
+                }
+            }
             .tag(2)
         }
         .accentColor(.systemBlack)
