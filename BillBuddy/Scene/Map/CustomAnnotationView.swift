1d39dba2a0ea8a733e57bfe65bdb0e1f853fbb6f
seungzunlee <dltmdwns312@gmail.com>
2023-12-19 19:03:27 +0900

    feat: 맵뷰 브랜치 이사

--- BillBuddy/Scene/Map/CustomAnnotationView.swift
+++ BillBuddy/Scene/Map/CustomAnnotationView.swift
@@ -6,42 +6,88 @@
 //
 
 import Foundation
+import SwiftUI
 import MapKit
 
 final class CustomAnnotationView: MKAnnotationView {
-    static let identifier = "CustomAnnotationView"
-
+    
+    lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [customImageView, pinIndexLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        return stack
+    }()
+    
+    lazy var customImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "customPinImage")
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    lazy var pinIndexLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.text = "INDEX"
+        label.textColor = .primary2
+        label.textAlignment = .center
+        return label
+    }()
+    
     override init(annotation: MKAnnotation?, reuseIdentifier: String?){
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-        frame = CGRect(x: 0, y: 0, width: 40, height: 50)
-        centerOffset = CGPoint(x: 0, y: -frame.size.height / 2)
-        setupUI()
+
+        setUpLayout()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupUI() {
-        backgroundColor = .clear
-    }
-}
-
-final class CustomAnnotation: NSObject, MKAnnotation {
-    let pinIndex: Int
-    let customPinImage: UIImage?
-    let coordinate: CLLocationCoordinate2D
-    
-    init(
-        pinIndex: Int,
-        customPinImage: UIImage?,
-        coordinate: CLLocationCoordinate2D
-    ) {
-        self.pinIndex = pinIndex
-        self.customPinImage = customPinImage
-        self.coordinate = coordinate
+    func setUpLayout() {
         
-        super.init()
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+                    stackView.topAnchor.constraint(equalTo: topAnchor),
+                    stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                    stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                    stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+                    
+                    pinIndexLabel.topAnchor.constraint(equalTo: stackView.topAnchor, constant: -15),
+                    
+                    customImageView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 0)
+                ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        customImageView.image = nil
+        pinIndexLabel.text = nil
+    }
+    
+    override func prepareForDisplay() {
+        super.prepareForDisplay()
+        
+        guard let annotation = annotation as? CustomAnnotation else { return }
+        
+        pinIndexLabel.text = String(annotation.pinIndex)
+        
+        let image = UIImage(named: "customPinImage")
+        
+        customImageView.image = image
+        
+        setNeedsLayout()
+        
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    // 레이아웃 사이즈 설정
     }
 }
 
