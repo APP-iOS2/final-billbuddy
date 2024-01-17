2011d59f16734af6377e4c6eddfa342a6ff1341d
seungzunlee <dltmdwns312@gmail.com>
2024-01-17 14:06:32 +0900

    feat: 앱 버전 체크 alert 추가

--- BillBuddy/Scene/App/BillBuddyApp.swift
+++ BillBuddy/Scene/App/BillBuddyApp.swift
@@ -87,9 +87,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         }
     // AppStore 이동
     func appUpdate() {
-        let appleId = "6474726564"        // 앱 스토어에 일반 정보의 Apple ID 입력
-        // UIApplication 은 Main Thread 에서 처리
-        DispatchQueue.main.async {
+        
+        let alert = UIAlertController(title: "업데이트 필수", message: "새 버전이 출시했습니다. 더 나은 서비스를 위해 업데이트를 해주세요.", preferredStyle: .alert)
+        
+        alert.addAction(.init(title: "업데이트", style: .default, handler: { _ in
+            let appleId = "6474726564"        // 앱 스토어에 일반 정보의 Apple ID 입력
             if let url = URL(string: "itms-apps://itunes.apple.com/app/\(appleId)"), UIApplication.shared.canOpenURL(url) {
                 if #available(iOS 10.0, *) {
                     UIApplication.shared.open(url, options: [:], completionHandler: nil)
@@ -97,7 +99,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
                     UIApplication.shared.openURL(url)
                 }
             }
-        }
+        }))
     }
 }
 
